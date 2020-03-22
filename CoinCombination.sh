41a1093b3cbbe5671ebc1a548c43b34bcc7ab91c
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-22 05:23:07 -0700

    Usecase5

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
 
+insertion () {
+	k=0
+	for i in $@
+	do
+		arr[$k]=`echo $i | awk -F. '{print $1}'`
+		k=$((k+1))
+	done
+	for ((i=1;i<$len;i++))
+	do
+		hole=${arr[$i]}
+		j=$((i-1))
+		while [ $j -ge 0 -a  $hole -le ${arr[$j]} ]
+		do
+			arr[$((j+1))]=${arr[$j]}
+			j=$((j-1))
+		done
+		arr[$((j+1))]=$hole
+	done
+	echo "Sorted Values are : "${arr[@]}
+	max=${arr[$((len-1))]}
+	for i in ${!dic[@]}
+	do
+		dicValue=`echo ${dic[$i]} | awk -F. '{print $1}'`
+		[ $dicValue -eq ${arr[$((len-1))]} ] && echo "Winning Pair is :" $i || continue
+	done
+}
+
 read -p"ENTER NO OF FLIP" flip
 declare -A dic
 dic[H]=0
@@ -18,12 +45,13 @@ do
 	fi
 done
 val=${dic[H]}
+
 echo "Percent of heads is :" 
-dic[H]=`echo "scale=2;$val/$flip*100"|bc`
+dic[H]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[H]} 
 val=${dic[T]}
 echo "Percent of tails is :" 
-dic[T]=`echo "scale=2;$val/$flip*100"|bc`
+dic[T]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[T]}
 dic[HH]=0
 dic[TT]=0
@@ -53,19 +81,19 @@ done
 
 echo "Percent of HH is :"
 val=${dic[HH]}
-dic[HH]=`echo "scale=2;$val/$flip*100"|bc`
+dic[HH]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[HH]}
 echo "Percent of HH is :"
 val=${dic[TT]}
-dic[TT]=`echo "scale=2;$val/$flip*100"|bc`
+dic[TT]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[TT]}
 echo "Percent of HH is :"
 val=${dic[HT]}
-dic[HT]=`echo "scale=2;$val/$flip*100"|bc`
+dic[HT]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[HT]}
 echo "Percent of HH is :"
 val=${dic[TH]}
-dic[TH]=`echo "scale=2;$val/$flip*100"|bc`
+dic[TH]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[TH]}
 
 dic[TTT]=0
@@ -117,40 +145,45 @@ done
 
 echo "Percent of TTT is :"
 val=${dic[TTT]}
-dic[TTT]=`echo "scale=2;$val/$flip*100"|bc`
+dic[TTT]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[TTT]}
 
 echo "Percent of TTH is :"
 val=${dic[TTH]}
-dic[TTH]=`echo "scale=2;$val/$flip*100"|bc`
+dic[TTH]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[TTH]}
 
 echo "Percent of THT is :"
 val=${dic[THT]}
-dic[THT]=`echo "scale=2;$val/$flip*100"|bc`
+dic[THT]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[THT]}
 
 echo "Percent of HTT is :"
 val=${dic[HTT]}
-dic[HTT]=`echo "scale=2;$val/$flip*100"|bc`
+dic[HTT]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[HTT]}
 
 echo "Percent of HTH is :"
 val=${dic[HTH]}
-dic[HTH]=`echo "scale=2;$val/$flip*100"|bc`
+dic[HTH]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[HTH]}
 
 echo "Percent of HHT is :"
 val=${dic[HHT]}
-dic[TTT]=`echo "scale=2;$val/$flip*100"|bc`
+dic[HHT]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[HHT]}
 
 echo "Percent of THH is :"
 val=${dic[THH]}
-dic[THH]=`echo "scale=2;$val/$flip*100"|bc`
+dic[THH]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[THH]}
 
 echo "Percent of HHH is :"
 val=${dic[HHH]}
-dic[HHH]=`echo "scale=2;$val/$flip*100"|bc`
+dic[HHH]=`echo "scale=1;$val/$flip*100"|bc`
 echo ${dic[HHH]}
+
+len=${#dic[@]}
+#len=$((len-1))
+
+insertion ${dic[@]}
