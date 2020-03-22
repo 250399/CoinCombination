a36dfcf0f699075cee029660802aa59c4bfc0253
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-22 03:01:38 -0700

    Use case2

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
 
-[ $((RANDOM%2)) -eq 1 ] && echo Heads || echo tails
+read -p"ENTER NO OF FLIP" flip
+declare -A dic
+dic[H]=0
+dic[T]=0
+for ((i=0;i<$flip;i++))
+do
+	r=$((RANDOM%2))
+	if [ $r -eq 0 ]
+	then
+		val=${dic[H]}
+		dic[H]=$((val+1))
+	else
+		val=${dic[T]}
+		dic[T]=$((val+1))
+	fi
+done
+val=${dic[H]}
+echo "Percent of heads is :"  
+echo "scale=2;$val/$flip*100"|bc 
+val=${dic[T]}
+echo "Percent of tails is :" 
+echo "scale=2;$val/$flip*100"|bc
